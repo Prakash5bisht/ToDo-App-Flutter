3ed5a5c517327492f1576dd60d6aae8e7f43be65
Prakash5bisht <singhbprakash505@gmail.com>
2020-06-24 10:00:22 -0700

    added ModalBottomSheet to add todo tasks

---
+++ lib/screens/task_dialog.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class TaskDialog extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Color(0xff757575),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(topLeft: Radius.circular(10.0), topRight: Radius.circular(10.0)),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(bottom: 150.0, top: 30.0, left: 30.0, right: 30.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+             Text('Add Task',
+             textAlign: TextAlign.center,
+             style: TextStyle(
+               color: Colors.black,
+               fontSize: 20.0,
+               fontWeight: FontWeight.w500
+             ),
+             ),
+             SizedBox(height: 20.0,),
+             TextField(
+               autofocus: true,
+               textAlign: TextAlign.center,
+               style: TextStyle(fontSize: 20.0),
+               decoration: InputDecoration(
+                 focusedBorder: UnderlineInputBorder(
+                   borderSide: BorderSide(color: Color(0xff595959)),
+                 )
+               ),
+               cursorColor: Colors.grey[500],
+             ),
+             SizedBox(height: 20.0,),
+             RaisedButton(
+               child: Text('ADD',
+               style: TextStyle(
+                 color: Colors.white,
+               ),
+               ),
+               color: Color(0xff1a1a1a),
+               onPressed: (){
+                 print('ok');
+               },
+             )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/task_screen.dart
+++ lib/screens/task_screen.dart
@@ -1,12 +1,33 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:to_do/widgets/task_list.dart';
+import 'package:to_do/screens/task_dialog.dart';
 
 
 class TasksScreen extends StatelessWidget {
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.deepOrangeAccent,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Color(0xff595959),
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+        onPressed: (){
+          showModalBottomSheet(context: context,
+              isScrollControlled: true,
+              builder: (BuildContext context) => SingleChildScrollView(
+                child: Container(
+                  padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+                  child: TaskDialog(),
+                ),
+              ));
+        },
+      ),
       body: SafeArea(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -19,7 +40,7 @@ class TasksScreen extends StatelessWidget {
                   CircleAvatar(
                     radius: 25.0,
                     backgroundColor: Colors.white,
-                    child: Icon(Icons.list,color: Colors.black,),
+                    child: Icon(Icons.list,color: Colors.black, size: 29.0,),
                   ),
                   SizedBox(height: 20.0,),
                   Text('ToDo',style: TextStyle(color: Colors.white,fontSize: 50.0,fontWeight: FontWeight.w700),),
@@ -28,10 +49,12 @@ class TasksScreen extends StatelessWidget {
             ),
             Expanded(
               child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
                 decoration: BoxDecoration(
                     color: Colors.white,
-                    borderRadius: BorderRadius.only(topLeft: Radius.circular(35.0),topRight: Radius.circular(35.0))
+                    borderRadius: BorderRadius.only(topLeft: Radius.circular(25.0),topRight: Radius.circular(25.0))
                 ),
+                child: TaskList(),
               ),
             ),
           ],
@@ -40,3 +63,6 @@ class TasksScreen extends StatelessWidget {
     );
   }
 }
+
+
+
